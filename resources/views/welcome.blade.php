29e722259e4af81b5a360fbe549424dac52b67d1
QuintinPeters <ps241606@summacollege.nl>
2023-09-07 16:16:31 +0200

    stage 1 front-end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,29 @@
     @vite('resources/css/app.css')
 </head>
 
-<body> 
-    <div>
-        <h1 class=" font-['poppins'] text-center text-xl font-medium">Spotify Analyser</h1>
+<body class="font-['poppins']">
+    <div class="flex flex-col h-[100vh] bg-stone-800">
+        <nav class="bg-stone-900">
+            <div class="basis-2/12 h-20 w-[100vw] flex items-center">
+                <a href="#" class="text-2xl font-medium mx-3 text-[#1bc257]">Spotify Analyser</a>
+                <ul class="text-white text-lg flex items-center mx-auto font-medium ">
+                    <li class="mx-3 hover:underline underline-offset-4 decoration-[#1bc257]"><a href="#">Home</a></li>
+                    <li class="mx-3 hover:underline underline-offset-4 decoration-[#1bc257]"><a href="#">Home</a></li>
+                    <li class="mx-3 hover:underline underline-offset-4 decoration-[#1bc257]"><a href="#">Home</a></li>
+                    <li class="mx-3 hover:underline underline-offset-4 decoration-[#1bc257]"><a href="#">Home</a></li>
+                    <li class="mx-3 hover:underline underline-offset-4 decoration-[#1bc257]"><a href="#">Home</a></li>
+                </ul>
+                <ion-icon name="person-circle-outline" class="mx-5 text-4xl text-white hover:text-[#1bc257]"></ion-icon>
+            </div>
+        </nav>
+        <div class=" basis-3/4">
+        </div>
+        <footer class="basis-2/12 bg-stone-900 text-white" >
+            <h1>yoooo</h1>
+        </footer>
     </div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
